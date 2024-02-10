d87add462c50037c1fd7f8705171301b42c5b587
Christelle Gévaert <christelle.gevaert32@gmail.com>
2024-02-11 00:40:08 +0100

    fix imports

---
+++ src/ui/navbar/components/SubItems.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { navbarLinks } from "@/app/ui/navbar/constants/navbarLinks";
+import { toggleSelected } from "@/app/ui/navbar/utils/toggleSelected";
+
+interface ISubItemsProps {
+    activeIndex: number;
+    setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
+    setIsMenuOpened: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const SubItems = ({
+    activeIndex,
+    setActiveIndex,
+    setIsMenuOpened,
+}: ISubItemsProps) => {
+    const handleMenuBurger = (index: number) => {
+        toggleSelected(index, activeIndex, setActiveIndex);
+        setIsMenuOpened(false);
+    };
+
+    return (
+        <div className="sticky z-40 flex flex-wrap justify-center text-grey-classic">
+            {navbarLinks.map((item, index) => (
+                <div
+                    key={index}
+                    className={`${index === activeIndex ? "text-grey-light" : "text-grey-disabled"} font-semibold hover:text-grey-light cursor-pointer px-5 py-2`}
+                >
+                    <Link
+                        href={item.path}
+                        onClick={() => handleMenuBurger(index)}
+                    >
+                        {item.label}
+                    </Link>
+                </div>
+            ))}
+        </div>
+    );
+};
