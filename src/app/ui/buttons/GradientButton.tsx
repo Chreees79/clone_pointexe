2c011d9020158058caf24e8d7b213b123e70d399
Christelle Gévaert <christelle.gevaert32@gmail.com>
2024-02-12 11:56:32 +0100

    fixed the size of border in GradientButton

--- src/app/ui/buttons/GradientButton.tsx
+++ src/app/ui/buttons/GradientButton.tsx
@@ -4,7 +4,7 @@ interface IGradientButtonProps {
 export const GradientButton = ({ label }: IGradientButtonProps) => {
     return (
         <div>
-            <button className="bg-gradient-to-r from-border-yellow to-border-orange text-white font-semibold rounded-full p-1 w-full">
+            <button className="bg-gradient-to-r from-border-yellow to-border-orange text-white font-semibold rounded-full p-[2px] w-full">
                 <span className="flex justify-center w-full bg-gray-900 rounded-full  hover:from-border-yellow hover:to-border-orange hover:bg-gradient-to-r hover:bg-cover hover:bg-no-repeat hover:bg-border-yellow hover:bg-opacity-100 hover:text-almost_black hover:font-semibold transition-all duration-700 text-white  py-2 px-4">
                     {label}
                 </span>
