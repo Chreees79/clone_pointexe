6b951ea830dd6c2812439ae640070ba5f315bdd6
Christelle Gévaert <christelle.gevaert32@gmail.com>
2024-02-13 01:21:45 +0100

    create ProcessSection of the home page and fix typo issue on hero

--- src/app/components/HomeComponent.tsx
+++ src/app/components/HomeComponent.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useInView } from "@react-spring/web";
-import { Hero } from "./Hero/Hero";
-import { Customers } from "./Customers/Customers";
+import { Hero } from "./hero/Hero";
+import { Customers } from "./customers/Customers";
+import { ProcessSection } from "./customers/ProcessSection";
 
 export const HomeComponent = () => {
     const [ref, inView] = useInView();
     const [ref2, inView2] = useInView();
+    const [ref3, inView3] = useInView();
 
     return (
         <>
@@ -30,6 +32,17 @@ export const HomeComponent = () => {
                 }}
             >
                 <Customers />
+            </div>
+            <div
+                ref={ref3}
+                className={` ${inView3 ? "animate-scaleAnimation" : ""} px-6`}
+                style={{
+                    opacity: inView ? 1 : 0.85,
+                    scale: inView ? 1 : 1,
+                    transition: "2s ease-in-out",
+                }}
+            >
+                <ProcessSection />
             </div>
         </>
     );
