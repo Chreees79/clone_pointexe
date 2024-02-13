c954e6f238bc537b6715ba09ead919b3dfab13bb
Christelle Gévaert <christelle.gevaert32@gmail.com>
2024-02-13 23:47:01 +0100

    add TechnologiesSection

---
+++ src/app/components/constants/TechnologiesList.ts
@@ -0,0 +1,66 @@
+import Comment from "../../../../public/comments-solid.svg";
+import Shield from "../../../../public/shield-solid.svg";
+import Gear from "../../../../public/gear-solid.svg";
+import DataBase from "../../../../public/database-solid.svg";
+import Cloud from "../../../../public/cloud-solid.svg";
+import ClipoardCheck from "../../../../public/clipboard-check-solid.svg";
+import CircleNode from "../../../../public/circle-nodes-solid.svg";
+import Light from "../../../../public/light.svg";
+import Wrench from "../../../../public/wrench.svg";
+
+export const TechnologiesList: {
+    image: string;
+    alt: string;
+    title: string;
+}[] = [
+    {
+        image: Gear,
+        alt: "icone de Développement",
+        title: "Développement",
+    },
+    {
+        image: Shield,
+        alt: "icone de Cybersecurité",
+        title: "Cybersecurité",
+    },
+    {
+        image: DataBase,
+        alt: "icone de Données",
+        title: "Données",
+    },
+    {
+        image: Comment,
+        alt: "icone de Conseils",
+        title: "Conseils",
+    },
+    {
+        image: Wrench,
+        alt: "icone de ERP et CRM",
+        title: "ERP et CRM",
+    },
+    {
+        image: Light,
+        alt: "icone de Technologies Innovantes",
+        title: "Technologies Innovantes",
+    },
+    {
+        image: ClipoardCheck,
+        alt: "icone de Project Management",
+        title: "Project Management",
+    },
+    {
+        image: Cloud,
+        alt: "icone de Cloud",
+        title: "Cloud",
+    },
+    {
+        image: Wrench,
+        alt: "icone de Support and Maintenance",
+        title: "Support and Maintenance",
+    },
+    {
+        image: CircleNode,
+        alt: "icone de Infrastructure",
+        title: "Infrastructure",
+    },
+];
