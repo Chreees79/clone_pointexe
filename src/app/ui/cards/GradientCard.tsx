2ed3ced4fe72d043dc1b4ab16b8711997f98d672
Christelle Gévaert <christelle.gevaert32@gmail.com>
2024-02-14 01:31:32 +0100

    create ExpertisesSection

--- src/app/ui/cards/GradientCard.tsx
+++ src/app/ui/cards/GradientCard.tsx
@@ -5,12 +5,14 @@ interface IGradientCardProps extends PropsWithChildren {
     size?: string;
     padding?: string;
     textContent?: boolean;
+    bgColor?: string;
 }
 
 export const GradientCard = ({
     type,
     size,
     padding,
+    bgColor,
     children,
 }: IGradientCardProps) => {
     return (
@@ -18,7 +20,7 @@ export const GradientCard = ({
             className={`${size && type === "rounded" ? `rounded-full ${size}` : "rounded"} bg-gradient-to-r from-border-yellow to-border-orange text-white font-semibold p-[2px] w-full`}
         >
             <div
-                className={`bg-background-dark rounded-full flex justify-center items-center ${padding}`}
+                className={`${bgColor ? bgColor : "bg-background-dark"} rounded-full flex justify-center items-center ${padding}`}
             >
                 {children}
             </div>
